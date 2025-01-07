39b240039bbc9ada125dee99dc2f087eaf81fb32
May <abeillee@bk.ru>
2025-01-07 10:31:30 +0300

    feat: add function createElement

---
+++ app.js
@@ -0,0 +1,17 @@
+let createElement = (settings) => {
+    const { tag = 'div', text = '', parent = document.body, classes = [] } = settings;
+
+    let element = document.createElement(tag);
+    element.textContent = text;
+
+    if (parent != null) {
+        parent.appendChild(element);
+    }
+
+    if (classes.length > 0) {
+        element.classList.add(...classes);
+    }
+
+    return element;
+}
+
